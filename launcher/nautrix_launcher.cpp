926a6c426b7b88c4181a0520c604a1fd42a73a23
mycroft440 <alquimistadodetalhe@gmail.com>
2026-08-27 16:40:30 -0300

    perf: wire adaptive resource policy into launcher

--- launcher/nautrix_launcher.cpp
+++ launcher/nautrix_launcher.cpp
@@ -92,17 +92,34 @@ bool FeatureEnabled(const std::string& mode, std::string_view feature_name) {
 std::vector<std::wstring> BuildInjectedArgs(const std::filesystem::path& latency_path) {
     SetEnv(L"NAUTRIX_TRADING_MODE", ReadKey(latency_path, "trading_mode", "automatic"));
     SetEnv(L"NAUTRIX_TRADING_SITES", ReadKey(latency_path, "trading_sites"));
+    SetEnv(L"NAUTRIX_CRITICAL_TRADING_SITES", ReadKey(latency_path, "critical_trading_sites"));
+    SetEnv(L"NAUTRIX_MEMORY_SAVER_ENABLED", ReadKey(latency_path, "enable_memory_saver", "1"));
+    SetEnv(L"NAUTRIX_MEMORY_SAVER_AGGRESSIVENESS", ReadKey(latency_path, "memory_saver_aggressiveness", "medium"));
+    SetEnv(L"NAUTRIX_MEMORY_SAVER_DISCARD_MINUTES", ReadKey(latency_path, "memory_saver_discard_minutes", "120"));
+    SetEnv(L"NAUTRIX_PRECONNECT_MAX_ORIGINS", ReadKey(latency_path, "preconnect_max_origins", "4"));
+    SetEnv(L"NAUTRIX_KEEPALIVE_MAX_ORIGINS", ReadKey(latency_path, "keepalive_max_origins", "2"));
     SetEnv(L"NAUTRIX_KEEPALIVE_ENABLED", ReadKey(latency_path, "enable_connection_keepalive", "1"));
     SetEnv(L"NAUTRIX_KEEPALIVE_IDLE_SECONDS", ReadKey(latency_path, "keepalive_idle_seconds", "120"));
     SetEnv(L"NAUTRIX_KEEPALIVE_PING_SECONDS", ReadKey(latency_path, "keepalive_ping_seconds", "25"));
     SetEnv(L"NAUTRIX_NETWORK_PRIORITY_BOOST", ReadKey(latency_path, "enable_network_priority_boost", "1"));
     SetEnv(L"NAUTRIX_SELECTIVE_THROTTLING_BYPASS", ReadKey(latency_path, "enable_selective_throttling_bypass", "1"));
+    SetEnv(L"NAUTRIX_BACKGROUND_CONNECTION_BYPASS", ReadKey(latency_path, "enable_background_connection_bypass", "1"));
     SetEnv(L"NAUTRIX_INTENT_PRECONNECT", ReadKey(latency_path, "enable_intent_preconnect", "1"));
     SetEnv(L"NAUTRIX_HIGH_RES_TIMER", ReadKey(latency_path, "enable_high_resolution_timer", "1"));
     SetEnv(L"NAUTRIX_FREEZING_PROTECTION", ReadKey(latency_path, "enable_freezing_protection", "1"));
     SetEnv(L"NAUTRIX_TRADING_PROCESS_PRIORITY", ReadKey(latency_path, "enable_trading_process_priority", "1"));
     SetEnv(L"NAUTRIX_DISABLE_ECOQOS", ReadKey(latency_path, "disable_ecoqos_for_trading", "1"));
     SetEnv(L"NAUTRIX_SPARE_RENDERER_WARMUP", ReadKey(latency_path, "enable_spare_renderer_warmup", "1"));
+
+    std::vector<std::wstring> injected_args;
+    const std::string blocker_mode = ReadKey(latency_path, "lightweight_blocker", "off");
+    if (FeatureEnabled(blocker_mode, "NautrixLightweightBlocker")) {
+        const auto blocker_dir = latency_path.parent_path().parent_path() /
+                                 L"extensions" / L"nautrix-blocker";
+        if (std::filesystem::is_regular_file(blocker_dir / L"manifest.json")) {
+            injected_args.emplace_back(L"--load-extension=" + blocker_dir.wstring());
+        }
+    }
 
     std::vector<std::string> features;
     const std::string optimistic = ReadKey(latency_path, "optimistic_dns_for_tcp", "ab");
@@ -118,13 +135,15 @@ std::vector<std::wstring> BuildInjectedArgs(const std::filesystem::path& latency
         features.emplace_back("EnableWebsocketsOverHttp3");
     }
 
-    if (features.empty()) return {};
-    std::wstring value = L"--enable-features=";
-    for (size_t i = 0; i < features.size(); ++i) {
-        if (i) value.push_back(L',');
-        value += Utf8ToWide(features[i]);
+    if (!features.empty()) {
+        std::wstring value = L"--enable-features=";
+        for (size_t i = 0; i < features.size(); ++i) {
+            if (i) value.push_back(L',');
+            value += Utf8ToWide(features[i]);
+        }
+        injected_args.push_back(std::move(value));
     }
-    return {std::move(value)};
+    return injected_args;
 }
 
 std::optional<std::wstring> ExtractShellSingleArgument() {
@@ -202,7 +221,7 @@ int NautrixLauncherMain(int argc, wchar_t** argv) {
     }
 
     std::vector<std::wstring> owned;
-    owned.reserve(static_cast<size_t>(argc) + 4);
+    owned.reserve(static_cast<size_t>(argc) + 6);
     const size_t copy_count = std::min(single_argument_index, static_cast<size_t>(argc));
     for (size_t i = 0; i < copy_count; ++i) owned.emplace_back(argv[i]);
 
